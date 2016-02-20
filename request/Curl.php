8fcd77c606a647f7c7cfc38ee651984ffc469a0b
Jonatas Sas <atendimento@jsas.com.br>
2016-02-20 11:22:29 -0200

    Inicial

---
+++ request/Curl.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace nox\request;
+
+    /**
+     * Class Curl
+     *
+     * @category Curl
+     * @author   Jonatas Sas
+     *
+     * @package  common\components\request
+     */
+    class Curl extends \Curl\Curl
+    {
+
+    }
---
+++ request/MultiCurl.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace common\components\request;
+
+    /**
+     * Class MultiCurl
+     *
+     * @category Curl
+     * @author   Jonatas Sas
+     *
+     * @package  common\components\request
+     */
+    class MultiCurl extends \Curl\MultiCurl
+    {
+
+    }
